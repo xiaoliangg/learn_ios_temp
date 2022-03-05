ebe2cbf5973a6ca9d9e0cb77ffe6da1c77ac370d
xiaoliangg <xiaoliangyuu@163.com>
2022-03-05 18:22:46 +0800

    CreatingAndCombiningViews_mine 新增地图和文字

--- CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/ContentView.swift
+++ CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/ContentView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-//            MapView()
-//                .ignoresSafeArea(edges: .top)
-//                .frame(height: 300)
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            // 可以使地图紧贴顶部
+            Spacer()
         }
     }
 }
