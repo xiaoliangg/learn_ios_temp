2cc4d73a95556c04ea6fb5d0cec31f4012004da5
xiaoliangg <xiaoliangyuu@163.com>
2022-03-05 17:21:10 +0800

    苹果官方教程 CreatingAndCombiningViews 备份:https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ TestSwift/TestSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSwift
+//
+//  Created by yl on 2022/3/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestSwift/TestSwift/TestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftApp.swift
+//  TestSwift
+//
+//  Created by yl on 2022/3/4.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
