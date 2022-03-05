e7614f9ed8b70efb605c6a3bd2fec9a50aeb9658
xiaoliangg <xiaoliangyuu@163.com>
2022-03-05 18:16:29 +0800

    CreatingAndCombiningViews_mine 新增图像

--- CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/ContentView.swift
+++ CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+//            MapView()
+//                .ignoresSafeArea(edges: .top)
+//                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+        }
     }
 }
 
