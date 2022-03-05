9276ee1e5aada6d6d5393cb8346a9d3b7bd7b5cd
xiaoliangg <xiaoliangyuu@163.com>
2022-03-05 18:56:41 +0800

    docs:CreatingAndCombiningViews_mine @state注解2

--- CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/MapView.swift
+++ CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/MapView.swift
@@ -12,6 +12,7 @@ import MapKit
 struct MapView: View {
     // @State解释:You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value.
     // 见 https://developer.apple.com/tutorials/swiftui/creating-and-combining-views section5
+    // @state 其他博客:https://onevcat.com/2021/01/swiftui-state/
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
