259c8647615537d087d0b6b648aadebeadff82a0
xiaoliangg <xiaoliangyuu@163.com>
2022-03-09 00:00:49 +0800

    BuildingListsAndNavigation_mine section2 新增LandmarkRow.swift

---
+++ BuildingListsAndNavigation_mine/BuildingListsAndNavigation_mine/Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  BuildingListsAndNavigation_mine
+//
+//  Created by yl on 2022/3/8.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
