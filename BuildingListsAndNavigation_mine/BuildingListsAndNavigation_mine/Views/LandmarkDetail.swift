5a2987dde4c9fec4b610146bc6ed62f2c8423644
xiaoliangg <xiaoliangyuu@163.com>
2022-03-09 00:32:38 +0800

    BuildingListsAndNavigation_mine section6 在ListView和DetailView之间使用NavigationView创建关联

---
+++ BuildingListsAndNavigation_mine/BuildingListsAndNavigation_mine/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  BuildingListsAndNavigation_mine
+//
+//  Created by yl on 2022/3/9.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
