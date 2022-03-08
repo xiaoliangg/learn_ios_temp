c13318813ff43e7dfd40b627837cb7ade71dc7fc
xiaoliangg <xiaoliangyuu@163.com>
2022-03-09 00:14:32 +0800

    BuildingListsAndNavigation_mine section4 创建一个静态的TableView

---
+++ BuildingListsAndNavigation_mine/BuildingListsAndNavigation_mine/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  BuildingListsAndNavigation_mine
+//
+//  Created by yl on 2022/3/9.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List{
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
