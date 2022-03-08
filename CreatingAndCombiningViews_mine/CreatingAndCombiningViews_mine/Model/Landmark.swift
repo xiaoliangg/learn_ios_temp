574d5f9825122f11b3a74195389e514b2af9f671
xiaoliangg <xiaoliangyuu@163.com>
2022-03-08 23:33:42 +0800

    BuildingListsAndNavigation_mine section1_Create a Landmark Model 暂时对启动后的效果没影响

---
+++ CreatingAndCombiningViews_mine/CreatingAndCombiningViews_mine/Model/Landmark.swift
@@ -0,0 +1,36 @@
+//
+//  Landmark.swift
+//  CreatingAndCombiningViews_mine
+//
+//  Created by yl on 2022/3/8.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+    private var coordinates: Coordinates
+    // 结构体初始化方法
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
