6b55778a4bf849494f8490ddcf0d8b4665cda16c
xiaoliangg <xiaoliangyuu@163.com>
2022-03-09 00:50:27 +0800

    BuildingListsAndNavigation_mine section8 在不同的设备上预览

--- BuildingListsAndNavigation_mine/BuildingListsAndNavigation_mine/Views/LandmarkList.swift
+++ BuildingListsAndNavigation_mine/BuildingListsAndNavigation_mine/Views/LandmarkList.swift
@@ -26,6 +26,13 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+//        LandmarkList()
+//            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+        
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
