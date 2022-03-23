d0dddcd388c6a4b168fb00d49d2c2e3d6bf56860
xiaoliangg <xiaoliangyuu@163.com>
2022-03-23 22:31:38 +0800

    Create a Favorite Button for Each Landmark @Binding

---
+++ HandingUserInput_mine/HandingUserInput_mine/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteButton.swift
+//  HandingUserInput_mine
+//
+//  Created by yl on 2022/3/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+        
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
