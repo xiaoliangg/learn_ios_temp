f2b9e1d7501767563f6530469a9355960a93113d
xiaoliangg <xiaoliangyuu@163.com>
2022-03-26 15:25:11 +0800

    Draw the Badge Symbol

---
+++ DrawingPathsAndShapes_mine/DrawingPathsAndShapes_mine/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  DrawingPathsAndShapes_mine
+//
+//  Created by yl on 2022/3/26.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
