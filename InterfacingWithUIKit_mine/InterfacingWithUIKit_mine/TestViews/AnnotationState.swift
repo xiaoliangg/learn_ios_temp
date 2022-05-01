8f955e79897ca8711c05873dfd6d87dec4108344
xiaoliangg <xiaoliangyuu@163.com>
2022-05-01 20:05:53 +0800

    @State 注解 添加注释

--- InterfacingWithUIKit_mine/InterfacingWithUIKit_mine/TestViews/AnnotationState.swift
+++ InterfacingWithUIKit_mine/InterfacingWithUIKit_mine/TestViews/AnnotationState.swift
@@ -3,11 +3,13 @@
 //  InterfacingWithUIKit_mine
 //
 //  Created by yl on 2022/4/4.
-//
+//  参考: https://onevcat.com/2021/01/swiftui-state/
 
 import SwiftUI
 
 struct AnnotationState: View {
+    // yl 经测试，此例中如果不用 @State 注解，下方 value += 1 会报错:
+    // Left side of mutating operator isn't mutable: 'self' is immutable 可变操作符的左侧不是可变的:self 是不可变的
     @State private var value = 99
     var body: some View {
         // @State变量基本用法
@@ -17,7 +19,7 @@ struct AnnotationState: View {
 //        }
         
         
-        // 1. 子视图中声明一个变量接收,子视图无需编写其他操作。父视图直接将 @State 类型修饰的变量穿参即可
+        // 1. 子视图中声明一个变量接收,子视图无需编写其他操作。父视图直接将 @State 类型修饰的变量传参即可
         // 2. 在 ContentView 中的 @State value 发生改变时，ContentView.body 被重新求值，DetailView 将被重新创建，包含新数字的 Text 被重新渲染。
         VStack(alignment: .leading) {
             DetailView(number: value)
