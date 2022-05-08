189079ec5a371a0307e91dc6353dcfd77566e791
xiaoliangg <xiaoliangyuu@163.com>
2022-05-08 12:56:08 +0800

    z01_swiftui_coredata_crud 1.新增数据 2.分析xcode选择use code data新增了哪些代码

--- z01_swiftui_coredata_crud/README.md
+++ z01_swiftui_coredata_crud/README.md
@@ -1,2 +1,3 @@
 # z01_swiftui_coredata_crud
 * 创建时注意勾选 use core data
+* xcdatamodeld 里声明的变量，需要重启xcode，才能在普通swift里使用而不报错
--- z01_swiftui_coredata_crud/z01_swiftui_coredata_crud/Persistence.swift
+++ z01_swiftui_coredata_crud/z01_swiftui_coredata_crud/Persistence.swift
@@ -10,28 +10,29 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
+//    static var preview: PersistenceController = {
+//        let result = PersistenceController(inMemory: true)
+//        let viewContext = result.container.viewContext
+//        for _ in 0..<10 {
+//            let newItem = Animals(context: viewContext)
+//            newItem.name="123"
+//        }
+//        do {
+//            try viewContext.save()
+//        } catch {
+//            // Replace this implementation with code to handle the error appropriately.
+//            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//            let nsError = error as NSError
+//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//        }
+//        return result
+//    }()
 
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "z01_swiftui_coredata_crud")
+//        container = NSPersistentContainer(name: "z01_swiftui_coredata_crud")
+        container = NSPersistentContainer(name: "test03")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
